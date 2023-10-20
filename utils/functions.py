d48a8a89f535fea59bdfccdb4ddf1fa8248838f7
IagoMagalhaes23 <iagomagalhaes23@gmail.com>
2023-10-19 21:36:36 -0300

    add documentation functions

--- utils/functions.py
+++ utils/functions.py
@@ -2,30 +2,19 @@
     Autores: Iago Magalhães e Vanessa Carvalho
     Data: 18/10/2023
     Descrição:
-        - 
+        - Implementa funções para leitura das imagens, aplicação de filtro de realce e composição do dataset para treino, teste e validação
 '''
 
 import os
 import cv2
-import random
 import pandas as pd
 import numpy as np
 
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-datagen = ImageDataGenerator(
-    rotation_range=10,
-    zoom_range = 0.1,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    horizontal_flip=False,
-    vertical_flip=False
-)
-
 def readFiles(caminhos):
     '''
-    
+        Função para ler todos os arquivos de imagem em uma pasta
+        :param caminhos: caminho dos arquivos de imagem
+        : return: retorna uma lista com o endereço e nome da imagem
     '''
     cont = 0
     data_list = []
@@ -39,7 +28,9 @@ def readFiles(caminhos):
 
 def filtro(cam):
     '''
-    
+        Função para redimensionamento da imagens em 224x224 pixels, conversão para tons de cinza e aplicação do filtro Gaussiano
+        :param cam: enderenço, nome e formato da imagem a ser tratada
+        :return: retorna uma imagem redimensionada em tons de cinza
     '''
     img = cv2.imread(cam)
     img = cv2.resize(img, (224, 224))
@@ -51,7 +42,9 @@ def filtro(cam):
 
 def compose_dataset(df):
     '''
-    
+        Função para compor o dataset de treino, teste e validação
+        :param df: recebe um dataframe com o endereço da imagem e seu label
+        :return: retorna dois np.arrays com a imagem e o label
     '''
     data = []
     labels = []
@@ -61,16 +54,3 @@ def compose_dataset(df):
         labels.append(label)
 
     return np.array(data), np.array(labels)
-
-def expandDataset(X_train, X_test, y_train, y_test, y_valid):
-    '''
-    
-    '''
-    X_train = datagen.fit(X_train)
-    X_test = datagen.fit(X_test)
-
-    y_train = to_categorical(y_train)
-    y_test = to_categorical(y_test)
-    y_valid = to_categorical(y_valid)
-
-    return X_train, X_test, y_train, y_test, y_valid
