85eb2c2a3239d8c71b51972f167f5949a159956f
IagoMagalhaes23 <iagomagalhaes23@gmail.com>
2023-10-23 20:32:43 -0300

    add functions in functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -9,9 +9,12 @@
 
 import os
 import cv2
+import itertools
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, roc_curve, auc
 
 def readFiles(caminhos):
     '''
@@ -80,3 +83,93 @@ def plot_hist(hist):
     plt.xlabel("epoch")
     plt.legend(["train", "validation"], loc="upper left")
     plt.show()
+
+def metrics(y_actual, y_pred, thresh):
+    '''
+        Função para plotagem das métricas de avaliação do modelo
+        :param y_actual: valor original da classe
+        :param y_pred: valor predito pelo modelo
+        :param thresh: limiar para ativação do valor
+        :return: retorna o valor de acurácia, precisão, sensibilidade, fpr, tpr, roc_auc
+    '''
+    accuracy = accuracy_score(y_actual, (y_pred > thresh))
+    precisao = precision_score(y_actual, y_pred)
+    sensibilidade = recall_score(y_actual, y_pred)
+    fpr, tpr, _ = roc_curve(y_actual, y_pred)
+    roc_auc = auc(fpr, tpr)
+    print('------------------------------------')
+    print('Acurácia:%.3f' %accuracy)
+    print('Precisão:%.3f' %precisao)
+    print('Sensibilidade:%.3f' %sensibilidade)
+    print('------------------------------------')
+    return accuracy, precisao, sensibilidade, fpr, tpr, roc_auc
+
+def plot_confusion_matrix(cm, target_names, title='Confusion matrix', cmap=None, normalize=True):
+    '''
+    
+    '''
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(10, 8))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title, fontsize=20)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45, fontsize=15)
+        plt.yticks(tick_marks, target_names, fontsize=15)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black",
+                     fontsize=30)
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black",
+                     fontsize=30)
+
+
+    plt.tight_layout()
+    plt.ylabel('Classificação correta', fontsize=20)
+    plt.xlabel('Predição', fontsize=20)
+    plt.show()
+
+# c=confusion_matrix(y_treino, (y_train_preds > 0.5))
+# plot_confusion_matrix(
+#     cm = c,
+#     normalize = False,
+#     target_names = ['Sol', 'Chuva'],
+#     title = "Matriz de confusão Naive Bayes"
+# )
+# plt.show()
+
+def roc_curve():
+    '''
+        Função para plotagem da curva roc
+    '''
+    # plt.figure()
+    # lw = 2
+    # plt.plot(acfpr, actpr, color='darkred',
+    #         lw=lw, label='CNN-IF + dropout - ROC curve (area = %0.2f)' % acroc_auc)
+    # plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    # plt.xlim([0.0, 1.0])
+    # plt.ylim([0.0, 1.05])
+    # plt.xlabel('False Positive Rate')
+    # plt.ylabel('True Positive Rate')
+    # plt.title('ROC curve of test data without diaphragm')
+    # plt.legend(loc="lower right")
+    # plt.show()
+    print('Tu ainda precisa fazer essa função')
