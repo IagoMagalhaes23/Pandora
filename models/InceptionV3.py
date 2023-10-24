747adc758d424e0d3b115ae2fa75f1f346fea100
IagoMagalhaes23 <iagomagalhaes23@gmail.com>
2023-10-24 11:22:31 -0300

    update InceptionV3.py

--- models/InceptionV3.py
+++ models/InceptionV3.py
@@ -16,7 +16,7 @@ def inceptionV3():
     '''
     model = Sequential()
 
-    model.add(Conv2D(32, (3, 3), input_shape=(224, 224, 3), activation='relu'))
+    model.add(Conv2D(32, (3, 3), input_shape=(224, 224, 1), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Conv2D(32, (3, 3), activation='relu'))
