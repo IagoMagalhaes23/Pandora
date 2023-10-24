427f98acdc4bffd4225b5d8ef1082923aa383fe5
IagoMagalhaes23 <iagomagalhaes23@gmail.com>
2023-10-24 15:56:50 -0300

    update YoloV5.py

--- models/Yolo/YoloV5.py
+++ models/Yolo/YoloV5.py
@@ -5,19 +5,19 @@
         - Implementação da rede YoloV5
 '''
 
-import os
+# import os
 
-def yoloV5():
-    '''
-        Função para executar o treinamento do Yolo V5
-    '''
-    os.system('python models/Yolo/yolov5/train.py --img 640 --batch-size 16 --epochs 5 --data models/Yolo/soc_mcq.v1i.yolov5pytorch/data.yaml')
+# def yoloV5():
+#     '''
+#         Função para executar o treinamento do Yolo V5
+#     '''
+#     os.system('python models/Yolo/yolov5/train.py --img 640 --batch-size 16 --epochs 5 --data models/Yolo/soc_mcq.v1i.yolov5pytorch/data.yaml')
 
-# import cv2 
-# from ultralytics import YOLO
+import cv2 
+from ultralytics import YOLO
 
-# im = cv2.imread('dataset/recortado/valid/0_A.png')
-# modelo = YOLO("models/Yolo/best.pt")
-# results = modelo.predict(im, conf=0.35)
-# for result in results:
-#   print(result.boxes.cls)
+im = cv2.imread('dataset/original/valid/4_jpg.rf.f644e9409057f80d13690f0bcbb32de5.jpg')
+modelo = YOLO("models/Yolo/best.pt")
+results = modelo.predict(im, conf=0.3, save=True)
+for result in results:
+  print(result.boxes.cls)
