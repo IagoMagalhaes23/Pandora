7f98d39de06cb73216b64b9dfff6671bee007b2e
IagoMagalhaes23 <iagomagalhaes23@gmail.com>
2023-10-24 14:13:28 -0300

    teste

--- models/Yolo/YoloV5.py
+++ models/Yolo/YoloV5.py
@@ -12,3 +12,12 @@ def yoloV5():
         Função para executar o treinamento do Yolo V5
     '''
     os.system('python models/Yolo/yolov5/train.py --img 640 --batch-size 16 --epochs 5 --data models/Yolo/soc_mcq.v1i.yolov5pytorch/data.yaml')
+
+# import cv2 
+# from ultralytics import YOLO
+
+# im = cv2.imread('dataset/recortado/valid/0_A.png')
+# modelo = YOLO("models/Yolo/best.pt")
+# results = modelo.predict(im, conf=0.35)
+# for result in results:
+#   print(result.boxes.cls)
